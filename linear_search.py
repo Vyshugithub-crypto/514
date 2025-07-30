1a17815a533e04f1e5fa72e72b2ccb6a4528567e
Vyshugithub-crypto <vaishnavikongalla7@gmail.com>
2025-07-30 19:05:17 +0530

    Delete linear_search.py

--- linear_search.py
+++
@@ -1,8 +0,0 @@
-def linearsearch(arr , target):
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return 1
-        return -1
-arr = [12, 33, 56, 67, 98]
-target = 56
-print(linearsearch(arr , target))
